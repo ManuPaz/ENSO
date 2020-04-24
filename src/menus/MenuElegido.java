e9a4563eec5bc22dd82b37ad695b6a6383205071
ManuPaz <manu.paz.pintor@gmail.com>
2020-04-24 11:37:45 +0200

    Commit

--- src/menus/MenuElegido.java
+++ src/menus/MenuElegido.java
@@ -14,4 +14,36 @@ public class MenuElegido {
 		this.bebida=bebida;
 	
 	}
+
+	public String getPrimero() {
+		return primero;
+	}
+
+	public void setPrimero(String primero) {
+		this.primero = primero;
+	}
+
+	public String getSegundo() {
+		return segundo;
+	}
+
+	public void setSegundo(String segundo) {
+		this.segundo = segundo;
+	}
+
+	public String getPostre() {
+		return postre;
+	}
+
+	public void setPostre(String postre) {
+		this.postre = postre;
+	}
+
+	public String getBebida() {
+		return bebida;
+	}
+
+	public void setBebida(String bebida) {
+		this.bebida = bebida;
+	}
 }
