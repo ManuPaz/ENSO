78981321005366d98066e5b434f23250ea0945b6
marti <marti@VivoBook-de-Martin>
2020-04-26 18:25:36 +0200

    Realizacion consultarMenuSemana().

--- src/datos/GestionDatos.java
+++ src/datos/GestionDatos.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 import menus.Menu;
@@ -117,8 +118,84 @@ public class GestionDatos implements InterfaceDeGestionDeDatos {
 
 	@Override
 	public ArrayList<Menu> consultarMenusSemana() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ArrayList<Menu> menusSemana = new ArrayList<>();
+		boolean encontradoHoy = false;
+		
+		Date hoy = new Date();
+		String[] hoyFormateado = hoy.toString().split(" ");
+		
+		String linea;
+        String[] partes;
+
+        try {
+            FileReader fr = new FileReader("src\\ficheros\\menus.txt");
+            BufferedReader br = new BufferedReader(fr);
+
+            while ((linea = br.readLine()) != null && menusSemana.size()!=5) {
+    
+                partes = linea.split(",");
+                if(partes.length==10) {
+                	String fechaMenu = partes[0];
+                	System.out.println(fechaMenu);
+                	if(fechaMenu.split(" ")[2].equals(hoyFormateado[2])&&fechaMenu.split(" ")[1].equals(hoyFormateado[1])&&fechaMenu.split(" ")[5].equals(hoyFormateado[5])) {
+                		ArrayList<String> primeros = new ArrayList<>();
+                		ArrayList<String> segundos = new ArrayList<>();
+                		ArrayList<String> postres = new ArrayList<>();
+                		
+                		primeros.add(partes[1]);
+                		primeros.add(partes[2]);
+                		primeros.add(partes[3]);
+                		
+                		segundos.add(partes[4]);
+                		segundos.add(partes[5]);
+                		segundos.add(partes[6]);
+                		
+                		postres.add(partes[7]);
+                		postres.add(partes[8]);
+                		postres.add(partes[9]);
+                		
+                		Menu menu = new Menu(primeros, segundos, postres, hoy);
+                		
+                		menusSemana.add(menu);
+                		encontradoHoy = true;
+                		
+                	}else if(encontradoHoy) {
+                		
+                		ArrayList<String> primeros = new ArrayList<>();
+                		ArrayList<String> segundos = new ArrayList<>();
+                		ArrayList<String> postres = new ArrayList<>();
+                		
+                		primeros.add(partes[1]);
+                		primeros.add(partes[2]);
+                		primeros.add(partes[3]);
+                		
+                		segundos.add(partes[4]);
+                		segundos.add(partes[5]);
+                		segundos.add(partes[6]);
+                		
+                		postres.add(partes[7]);
+                		postres.add(partes[8]);
+                		postres.add(partes[9]);
+                		
+                		Menu menu = new Menu(primeros, segundos, postres, hoy);
+                		
+                		menusSemana.add(menu);
+                	}
+                }
+            }
+            
+            br.close();
+            fr.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        if(menusSemana.size()>0) {
+        	return menusSemana;
+        }else {
+        	return null;
+        }
 	}
 
 	@Override
