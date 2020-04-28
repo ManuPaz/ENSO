7f82f83e8619222b1e8585d7d7f7574907354d32
Noa <Noa@DESKTOP-RFOOOFL>
2020-04-28 12:03:14 +0200

    Cambio principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,3 @@
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 import Excepciones.*;
@@ -33,43 +30,14 @@ public class Principal {
 		GestionPagos gP = new GestionPagos(iS);
 		InterfaceGestionDePagosYCalificaciones iGPC = gP;
 		
+		SeleccionMenus sM = new SeleccionMenus(iGPC);
+		InterfaceSeleccionMenus iSM = sM;
+		
+		
 		Date fecha= new Date();
-
-		// SUBSISTEMA DE GESTION DE MENÚS
-		//Obtener menú del día
-		iGM.obtenerMenuDia();
-		
-		//Consultar menú semanal
-		iGM.consultarMenusSemana();
-		
-		//Obtener platos disponibles en el menú
-		iGM.obtenerPlatosDisponibles("primero");
-		iGM.obtenerPlatosDisponibles("segundo");
-		iGM.obtenerPlatosDisponibles("postre");
-		
-		//Construir menú del día
-		ArrayList<String> primerosPlatos = new ArrayList<>();
-		ArrayList<String> segundosPlatos = new ArrayList<>();
-		ArrayList<String> platosPostres = new ArrayList<>();
-		primerosPlatos.add("Sopa");
-		primerosPlatos.add("Empanada");
-		primerosPlatos.add("Ensaladilla");
-		segundosPlatos.add("Lasaña de verduras");
-		segundosPlatos.add("Raxo con patatas");
-		segundosPlatos.add("Merluza a la gallega");
-		platosPostres.add("Arroz con leche");
-		platosPostres.add("Natillas");
-		platosPostres.add("Fruta");
-		try {
-			iGM.construirMenuDia(fecha, primerosPlatos, segundosPlatos, platosPostres);
-		} catch (FechaIncorrectaExcepcion | PlatoIncorrectoExcepcion e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
 		
 
-		// SUBSISTEMA DE ANÁLISIS Y ESTADÍSTICAS
+		// SUBSISTEMA DE ANALISIS Y ESTADISTICAS
 		iAE.horaMasFrecuente();
 		iAE.ocupacionComedor();
 		iAE.platoMasPedido();
@@ -87,28 +55,27 @@ public class Principal {
 		// Insertar factura
 		MenuElegido menuElegido = new MenuElegido("Sopa de fideos", "Lubina a la plancha", "Flan", "Agua con gas");
 		Factura factura = new Factura(6, 90, 123, menuElegido);
-		gD.insertarFactura(factura);
-		gD.insertarFactura(null);
+		iGD.insertarFactura(factura);
+		iGD.insertarFactura(null);
 
 		// Consultar platos
-		ArrayList<Plato> platos = new ArrayList<>();
-		gD.consultarPlatos();
+		iGD.consultarPlatos();
 
 		// Insertar valoraciones
 		Date ahora = new Date();
 		Date despues = new Date();
 		Valoracion valoracion = new Valoracion(ahora, despues, 123, "Flan", 5);
-		gD.insertarValoracion(valoracion);
-		gD.insertarValoracion(null);
+		iGD.insertarValoracion(valoracion);
+		iGD.insertarValoracion(null);
 
 		// Consultar valoraciones
-		gD.consultarValoraciones();
+		iGD.consultarValoraciones();
 
 		// Consultar Menu Semana
-		gD.consultarMenusSemana();
+		iGD.consultarMenusSemana();
 
 		// Consultar Menu Hoy
-		Menu menu = gD.consultarMenuHoy();
+		Menu menu = iGD.consultarMenuHoy();
 
 		// Insertar menu
 		ArrayList<String> primeros = new ArrayList<>();
@@ -117,7 +84,7 @@ public class Principal {
 		primeros.add("Sopa");
 		primeros.add("Empanada");
 		primeros.add("Ensaladilla");
-		segundos.add("Lasaña de verduras");
+		segundos.add("Lasanha de verduras");
 		segundos.add("Raxo con patatas");
 		segundos.add("Merluza a la gallega");
 		postres.add("Arroz con leche");
@@ -125,10 +92,80 @@ public class Principal {
 		postres.add("Fruta");
 		ahora = new Date();
 		menu = new Menu(primeros, segundos, postres, ahora);
-		gD.insertarMenu(menu);
+		iGD.insertarMenu(menu);
 
 		// Consultar Factura
-		gD.consultarFactura();
+		iGD.consultarFactura();
+		
+		// SUBSISTEMA DE GESTION DE MENUS
+		//Obtener menu del dia
+		iGM.obtenerMenuDia();
+		
+		//Consultar menu semanal
+		iGM.consultarMenusSemana();
+		
+		//Obtener platos disponibles en el menu
+		iGM.obtenerPlatosDisponibles("primero");
+		iGM.obtenerPlatosDisponibles("segundo");
+		iGM.obtenerPlatosDisponibles("postre");
+		
+		//Construir menu del dia
+		ArrayList<String> primerosPlatos = new ArrayList<>();
+		ArrayList<String> segundosPlatos = new ArrayList<>();
+		ArrayList<String> platosPostres = new ArrayList<>();
+		primerosPlatos.add("Fabada");
+		primerosPlatos.add("Empanada");
+		primerosPlatos.add("Ensaladilla");
+		segundosPlatos.add("Paella");
+		segundosPlatos.add("Pollo al limon");
+		segundosPlatos.add("Merluza al horno");
+		platosPostres.add("Arroz con leche");
+		platosPostres.add("Natillas");
+		platosPostres.add("Manzana");
+		try {
+			iGM.construirMenuDia(fecha, primerosPlatos, segundosPlatos, platosPostres);
+		} catch (FechaIncorrectaExcepcion | PlatoIncorrectoExcepcion e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		//SUBSITEMA DE SELECCION DE MENUS
+		String primero="Sopa";
+		String segundo="Lasaña de verduras";
+		String bebida="Agua";
+		String postre="Fruta";
+		
+		try {
+			iSM.seleccionarMenu(primero, segundo, bebida, postre);
+		} catch (PlatoIncorrectoExcepcion e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			iSM.ofrecerMenu();
+		} catch (NoHayMenuExcepcion e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		iSM.ofrecerBebidas();
+		
+		//SUBSISTEMA DE GESTION DE PAGOS Y CALIFICACIONES 
+		//Creamos el menu
+		MenuElegido menuElegido2 = new MenuElegido ("Croquetas","Paella","Natillas","Agua");
+		iGPC.nuevoMenuPedido(menuElegido2);
+		//Simulamos la devolucion de la bandeja ayudandonos del subsistema de sensores
+		int codigo= s.getIdBandeja().get(1);
+		Date hora = new Date();
+		iGPC.bandejaDevuelta(hora, codigo);
+		iS.simularDevolucionBandeja(iGPC, codigo);
+		//Valoramos un plato consumido
+		iGPC.valoracion(10, "Croquetas" ,codigo);
+
+		//SUBSISTEMA DE SENSORES
+		iS.devolverIdentificadorBandeja();
+		iS.devolverIdentificadorVale();
 
 	}
 
